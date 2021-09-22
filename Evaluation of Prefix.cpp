0e9634acdc9556db508307b5b448cc510dcf05c0
kunal_cpp <kunaliesc@gmail.com>
2021-09-22 15:44:35 +0530

    Update Evaluation of Prefix.cpp

--- Evaluation of Prefix.cpp
+++ Evaluation of Prefix.cpp
@@ -1,1 +1,29 @@
+int solve(int num1, int num2, char opr){
+    
+    if(opr == '+') return num1 + num2;
+    else if (opr == '-') return num1 - num2;
+    else if(opr == '*') return num1*num2;
+    else if(opr == '/') return num2/num1;
+    
+return -1;
+}
+
+int evaluationofPrefix(string &expression) {
+
+    stack<int> stk;
+    string str;
+    for(int i = s.size()-1; i >= 0; i++){
+
+        if(s[i] >= '0' && s[i] <= '9'){
+            stk.push(s[i]- '0');
+        } else{ // operator
+            int op1 = stk.top(); stk.pop();
+            int op2 = stk.top(); stk.pop();
+            int res = solve(op1, op2, s[i]);
+
+            stk.push(res);
+        }
+    }   
+return stk.top();
+}
 
