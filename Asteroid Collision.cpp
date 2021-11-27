3a08ea014e006eb5bc7c7aad2ee83a5a05bf66c7
kunal_cpp <kunaliesc@gmail.com>
2021-11-27 10:33:09 +0530

    Update Asteroid Collision.cpp

--- Asteroid Collision.cpp
+++ Asteroid Collision.cpp
@@ -4,10 +4,11 @@ Problem Link: https://leetcode.com/problems/asteroid-collision/
 Asteroid Collision
 
 We are given an array asteroids of integers representing asteroids in a row.
-For each asteroid, the absolute value represents its size, and the sign represents its direction (positive meaning right, negative meaning left). Each asteroid moves at the same
-speed.
-Find out the state of the asteroids after all collisions. If two asteroids meet, the smaller one will explode. If both are the same size, both will explode. Two asteroids moving
-in the same direction will never meet.
+For each asteroid, the absolute value represents its size, and the sign represents its direction (positive meaning right, negative meaning left). 
+Each asteroid moves at the same speed.
+
+Find out the state of the asteroids after all collisions. If two asteroids meet, the smaller one will explode. If both are the same size, both will
+explode. Two asteroids moving in the same direction will never meet.
 
 Example 1:
 Input: asteroids = [5,10,-5]
@@ -70,3 +71,5 @@ public:
     return v;
     }
 };
+
+
