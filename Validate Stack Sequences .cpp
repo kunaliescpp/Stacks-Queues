903ceac37688482acf71561c9276ee9b531299cb
kunal_cpp <kunaliesc@gmail.com>
2021-12-04 10:24:45 +0530

    Update Validate Stack Sequences .cpp

--- Validate Stack Sequences .cpp
+++ Validate Stack Sequences .cpp
@@ -2,13 +2,14 @@
 
 Validate Stack Sequences
 
-Given two integer arrays pushed and popped each with distinct values, return true if this could have been the result of a sequence of push and pop operations on an initially 
-empty stack, or false otherwise.
+Given two integer arrays pushed and popped each with distinct values, return true if this could have been the result of a sequence of
+push and pop operations on an initially empty stack, or false otherwise.
 
 Example 1:
 Input: pushed = [1,2,3,4,5], popped = [4,5,3,2,1]
 Output: true
-Explanation: We might do the following sequence: push(1), push(2), push(3), push(4), pop() -> 4, push(5), pop() -> 5, pop() -> 3, pop() -> 2, pop() -> 1
+Explanation: We might do the following sequence: push(1), push(2), push(3), push(4), pop() -> 4, push(5), pop() -> 5, pop() -> 3, 
+pop() -> 2, pop() -> 1
 
 Example 2:
 Input: pushed = [1,2,3,4,5], popped = [4,3,5,1,2]
@@ -17,7 +18,6 @@ Explanation: 1 cannot be popped before 2.
  
 
 Constraints:
-
 1 <= pushed.length <= 1000
 0 <= pushed[i] <= 1000
 All the elements of pushed are unique.
@@ -43,3 +43,5 @@ public:
     return (pop_idx == popped.size());
     }
 };
+
+
