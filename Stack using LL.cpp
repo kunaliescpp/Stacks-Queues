e189a0f81070928963798719a3837066596c8ab6
kunal_cpp <kunaliesc@gmail.com>
2022-03-05 23:57:44 +0530

    Update Stack using LL.cpp

--- Stack using LL.cpp
+++ Stack using LL.cpp
@@ -1,1 +1,30 @@
+class stack{
+    // stack using ll
+    class Node{
+        public:
+            Node* next;
+            
+            Node(_val){
+              int val = _val;
+              next = NULL;
+            }
+    };
+    
+    Node* head = NULL;
+    void push(int _val){
+        Node* newnode = new Node(_val);
+        newnode->next = head;
+        head = newnode;
+    }
+    
+    void pop(){
+        if(head == NULL) return false;
+        
+        Node* curr = head;
+        head = curr->next;
+        curr->next = NULL;
+        
+    return true;
+    }
+};
 
